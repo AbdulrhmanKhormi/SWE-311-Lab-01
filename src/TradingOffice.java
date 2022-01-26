31f7aae7ab07df27b5c137a260478e0e3b2651e0
AbdulrhmanKhormi <75221142+AbdulrhmanKhormi@users.noreply.github.com>
2022-01-26 22:32:24 +0300

    adding the SMA method

--- src/TradingOffice.java
+++ src/TradingOffice.java
@@ -39,13 +39,26 @@ public class TradingOffice {
         else
                 System.out.println("the date does not exist");
 
-        ArrayList<Double> EMAs = EMA(AdjClose,50.0,2.0,0.0);
-        while (!EMAs.isEmpty()){
+        //ArrayList<Double> EMAs = EMA(AdjClose,50.0,2.0,0.0);
+        /*while (!EMAs.isEmpty()){
             System.out.println(EMAs.remove(0));
+        }*/
+        ArrayList<Double> SMAs = SMA(new ArrayList<>(),10.0,AdjClose);
+        while (!SMAs.isEmpty()){
+            System.out.println(SMAs.remove(0));
         }
 }
-    public Double SMA(){
-            return 0.0;
+    public static ArrayList<Double> SMA(ArrayList<Double> SMA, Double days, ArrayList<Double> Values){
+        if (Values.isEmpty())
+            return SMA;
+        Double PeriodSum = 0.0;
+        for (int i = 0; i < days; i++) {
+            if (Values.isEmpty())
+                break;
+            PeriodSum+=Values.remove(0);
+        }
+        SMA.add(Math.abs((days-PeriodSum)/days));
+        return SMA(SMA,days,Values);
     }
     public static ArrayList<Double> EMA(ArrayList<Double> Values, Double days, Double Smoothing, Double PreEMA){
         ArrayList<Double> EMA = new ArrayList<>();
